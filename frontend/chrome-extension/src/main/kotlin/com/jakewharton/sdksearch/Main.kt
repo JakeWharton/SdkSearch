846c0cfda41c189f13e7b9cc29d2ce85405c5748
Jake Wharton <jakew@google.com>
2018-01-06 22:57:41 -0500

    Get reference list fetching proof-of-concept working.

--- frontend/chrome-extension/src/main/kotlin/com/jakewharton/sdksearch/Main.kt
+++ frontend/chrome-extension/src/main/kotlin/com/jakewharton/sdksearch/Main.kt
@@ -4,7 +4,9 @@ import com.jakewharton.sdksearch.chrome.DefaultSuggestResult
 import com.jakewharton.sdksearch.chrome.UpdateProperties
 import com.jakewharton.sdksearch.chrome.omnibox
 import com.jakewharton.sdksearch.chrome.tabs
+import com.jakewharton.sdksearch.reference.ITEM_LIST_URL_PATHS
 import com.jakewharton.sdksearch.reference.PRODUCTION_DAC
+import kotlin.browser.window
 
 fun main(vararg args: String) {
   omnibox.setDefaultSuggestion(DefaultSuggestResult("Search Android SDK docs for <match>%s</match>"))
@@ -27,5 +29,25 @@ fun main(vararg args: String) {
     }
 
     tabs.update(UpdateProperties(url = url))
+  }
+
+
+  for ((name, path) in ITEM_LIST_URL_PATHS) {
+    val url = PRODUCTION_DAC + path
+    println("Fetching $name $url…")
+    window.fetch(url).then {
+      if (it.status != 200.toShort()) {
+        println("Failed")
+      } else {
+        it.text().then {
+          // Data starts with a "val SOMETHING =" prefix which we skip.
+          val startIndex = it.indexOf('=') + 1
+          // Data ends with a ";<newline>" suffix which we skip.
+          val endIndex = it.lastIndexOf(';')
+
+          println(it.substring(startIndex, endIndex))
+        }
+      }
+    }
   }
 }
