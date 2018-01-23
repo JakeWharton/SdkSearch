a238122cbfc279b05fe87cf75e963799f24272ee
Jake Wharton <jakew@google.com>
2018-01-23 09:23:04 -0500

    Add rudimentary filtering of items into the Omnibox.

--- store/js/src/main/kotlin/com/jakewharton/sdksearch/store/ItemStore.kt
+++ store/js/src/main/kotlin/com/jakewharton/sdksearch/store/ItemStore.kt
@@ -2,4 +2,5 @@ package com.jakewharton.sdksearch.store
 
 actual interface ItemStore {
   actual suspend fun updateListing(listing: String, items: List<Item>)
+  suspend fun queryItems(term: String): List<Item>
 }
