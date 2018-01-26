2284de4041ad43d4056d5f1648496c428a3e0f17
Jake Wharton <jakew@google.com>
2018-01-25 23:23:46 -0500

    Some easy store tests.

--- store/android/src/androidTest/java/com/jakewharton/sdksearch/store/ItemStoreTest.kt
+++ store/android/src/androidTest/java/com/jakewharton/sdksearch/store/ItemStoreTest.kt
@@ -5,6 +5,8 @@ import io.reactivex.observers.TestObserver
 import io.reactivex.schedulers.Schedulers
 import kotlinx.coroutines.experimental.runBlocking
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class ItemStoreTest {
@@ -15,14 +17,81 @@ class ItemStoreTest {
       .build()
       .itemStore()
 
-  @Test fun wildcards() {
-    runBlocking {
-      itemStore.updateItems(listOf(
-          Item(1, "com.example", "One%Two", false, "percent.html"),
-          Item(2, "com.example", "One_Two", false, "underscore.html"),
-          Item(3, "com.example", "One\\Two", false, "escape.html")
-      ))
+  @Test fun query() = runBlocking {
+    itemStore.updateItems(listOf(
+        Item.createForInsert("com.example.One", "one.html", false)
+    ))
+
+    itemStore.queryItems("One")
+        .test()
+        .takeValue {
+          val item = it.single()
+          assertEquals("com.example", item.packageName)
+          assertEquals("One", item.className)
+          assertFalse(item.deprecated)
+          assertEquals("one.html", item.link)
+        }
+        .dispose()
+  }
+
+  @Test fun upsert() = runBlocking {
+    itemStore.updateItems(listOf(
+        Item.createForInsert("com.example.One", "one.html", false)
+    ))
+
+    val query = itemStore.queryItems("One").test()
+
+    var id: Long = -1L
+    query.takeValue {
+      val item = it.single()
+      id = item.id
+      assertEquals("com.example", item.packageName)
+      assertEquals("One", item.className)
+      assertEquals("one.html", item.link)
+      assertFalse(item.deprecated)
     }
+
+    itemStore.updateItems(listOf(
+        Item.createForInsert("com.example.One", "two.html", true)
+    ))
+
+    query.takeValue {
+      val item = it.single()
+      assertEquals(id, item.id)
+      assertEquals("com.example", item.packageName)
+      assertEquals("One", item.className)
+      assertEquals("two.html", item.link)
+      assertTrue(item.deprecated)
+    }
+    query.dispose()
+  }
+
+  @Test fun count() = runBlocking {
+    val query = itemStore.count().test()
+
+    query.takeValue { assertEquals(0, it) }
+
+    itemStore.updateItems(listOf(
+        Item.createForInsert("com.example.One", "one.html", false)
+    ))
+
+    query.takeValue { assertEquals(1, it) }
+
+    itemStore.updateItems(listOf(
+        Item.createForInsert("com.example.Two", "two.html", false),
+        Item.createForInsert("com.example.Three", "three.html", false)
+    ))
+
+    query.takeValue { assertEquals(3, it) }
+    query.dispose()
+  }
+
+  @Test fun wildcards() = runBlocking {
+    itemStore.updateItems(listOf(
+        Item.createForInsert("com.example.One%Two", "percent.html", false),
+        Item.createForInsert("com.example.One_Two", "underscore.html", false),
+        Item.createForInsert("com.example.One\\Two", "escape.html", false)
+    ))
 
     itemStore.queryItems("%")
         .test()
