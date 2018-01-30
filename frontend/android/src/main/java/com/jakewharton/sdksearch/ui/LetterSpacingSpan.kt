9bf00c824a70d4eceba68d9528091cf58a87045c
Jake Wharton <jakew@google.com>
2018-01-29 23:40:31 -0500

    Internal.

--- frontend/android/src/main/java/com/jakewharton/sdksearch/ui/LetterSpacingSpan.kt
+++ frontend/android/src/main/java/com/jakewharton/sdksearch/ui/LetterSpacingSpan.kt
@@ -3,7 +3,7 @@ package com.jakewharton.sdksearch.ui
 import android.text.TextPaint
 import android.text.style.MetricAffectingSpan
 
-class LetterSpacingSpan(private val letterSpacing: Float = 0.0f) : MetricAffectingSpan() {
+internal class LetterSpacingSpan(private val letterSpacing: Float = 0.0f) : MetricAffectingSpan() {
   override fun updateDrawState(tp: TextPaint) {
     tp.letterSpacing = letterSpacing
   }
