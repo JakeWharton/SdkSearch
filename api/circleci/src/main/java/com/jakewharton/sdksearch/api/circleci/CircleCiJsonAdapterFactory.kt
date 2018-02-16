9e4621be3aacb2f7f15fb5668fea09f70e00b9cf
Jake Wharton <jakew@google.com>
2018-02-16 14:37:58 -0500

    Switch to Kotshi 1.1.0-SNAPSHOT and old factory style.

--- api/circleci/src/main/java/com/jakewharton/sdksearch/api/circleci/CircleCiJsonAdapterFactory.kt
+++ api/circleci/src/main/java/com/jakewharton/sdksearch/api/circleci/CircleCiJsonAdapterFactory.kt
@@ -1,6 +1,11 @@
 package com.jakewharton.sdksearch.api.circleci
 
+import com.squareup.moshi.JsonAdapter
 import se.ansman.kotshi.KotshiJsonAdapterFactory
 
 @KotshiJsonAdapterFactory
-internal object CircleCiJsonAdapterFactory : KotshiCircleCiJsonAdapterFactory()
+internal abstract class CircleCiJsonAdapterFactory : JsonAdapter.Factory {
+  companion object {
+    @JvmField val INSTANCE: CircleCiJsonAdapterFactory = KotshiCircleCiJsonAdapterFactory()
+  }
+}
