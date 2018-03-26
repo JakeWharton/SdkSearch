32cfaeaa60d3e0f348b542cba1034b2dd802620d
Jake Wharton <jakew@google.com>
2018-03-26 11:32:08 -0400

    Use the config to provide the DAC base url host.

--- frontend/chrome-extension/background/src/main/kotlin/com/jakewharton/sdksearch/background/Background.kt
+++ frontend/chrome-extension/background/src/main/kotlin/com/jakewharton/sdksearch/background/Background.kt
@@ -10,70 +10,78 @@ import com.jakewharton.sdksearch.api.dac.BaseUrl
 import com.jakewharton.sdksearch.api.dac.FetchDocumentationService
 import com.jakewharton.sdksearch.reference.ITEM_LIST_URL_PATHS
 import com.jakewharton.sdksearch.reference.PRODUCTION_DAC
+import com.jakewharton.sdksearch.reference.PRODUCTION_GIT_WEB
 import com.jakewharton.sdksearch.store.Item
+import com.jakewharton.sdksearch.store.StorageAreaConfigStore
 import com.jakewharton.sdksearch.store.StorageAreaItemStore
 import kotlinx.coroutines.experimental.CoroutineStart.UNDISPATCHED
 import kotlinx.coroutines.experimental.Job
 import kotlinx.coroutines.experimental.launch
 
 fun main(vararg args: String) {
-  omnibox.setDefaultSuggestion(
-      DefaultSuggestResult("Search Android SDK docs for <match>%s</match>"))
+  val itemStore = StorageAreaItemStore(storage.local)
+  val configStore = StorageAreaConfigStore(storage.sync, PRODUCTION_GIT_WEB, PRODUCTION_DAC)
 
-  val store = StorageAreaItemStore(storage.local)
-  val baseUrl = BaseUrl(PRODUCTION_DAC)
+  launch {
+    val config = configStore.load()
 
-  var queryJob: Job? = null
-  omnibox.onInputChanged.addListener { text, suggestions ->
-    queryJob?.cancel()
-    if (text.isEmpty()) {
-      return@addListener
+    omnibox.setDefaultSuggestion(
+        DefaultSuggestResult("Search Android SDK docs for <match>%s</match>"))
+
+    val baseUrl = BaseUrl(config.dacUrl)
+
+    var queryJob: Job? = null
+    omnibox.onInputChanged.addListener { text, suggestions ->
+      queryJob?.cancel()
+      if (text.isEmpty()) {
+        return@addListener
+      }
+
+      queryJob = launch(start = UNDISPATCHED) {
+        println("Searching $text")
+
+        val items = itemStore.queryItems(text)
+            .take(5)
+            .map {
+              val matchStart = it.className.indexOf(text, ignoreCase = true)
+              val matchEnd = matchStart + text.length
+              val description = buildString {
+                append("<dim>")
+                append(it.packageName)
+                append(".</dim>")
+                append(it.className.substring(0, matchStart))
+                append("<match>")
+                append(it.className.substring(matchStart, matchEnd))
+                append("</match>")
+                append(it.className.substring(matchEnd))
+              }
+              SuggestResult(baseUrl.resolve(it.link), description, false)
+            }.toTypedArray()
+        suggestions(items)
+      }
     }
 
-    queryJob = launch(start = UNDISPATCHED) {
-      println("Searching $text")
+    omnibox.onInputEntered.addListener { text, _ ->
+      val url = if (text.startsWith("http://") || text.startsWith("https://")) {
+        text
+      } else {
+        "${config.dacUrl}index.html?q=$text"
+      }
 
-      val items = store.queryItems(text)
-          .take(5)
-          .map {
-            val matchStart = it.className.indexOf(text, ignoreCase = true)
-            val matchEnd = matchStart + text.length
-            val description = buildString {
-              append("<dim>")
-              append(it.packageName)
-              append(".</dim>")
-              append(it.className.substring(0, matchStart))
-              append("<match>")
-              append(it.className.substring(matchStart, matchEnd))
-              append("</match>")
-              append(it.className.substring(matchEnd))
-            }
-            SuggestResult(baseUrl.resolve(it.link), description, false)
-          }.toTypedArray()
-      suggestions(items)
-    }
-  }
-
-  omnibox.onInputEntered.addListener { text, _ ->
-    val url = if (text.startsWith("http://") || text.startsWith("https://")) {
-      text
-    } else {
-      "${PRODUCTION_DAC}index.html?q=$text"
+      tabs.update(UpdateProperties(url = url))
     }
 
-    tabs.update(UpdateProperties(url = url))
-  }
+    val service = FetchDocumentationService(baseUrl)
 
-  val service = FetchDocumentationService(baseUrl)
+    ITEM_LIST_URL_PATHS.forEach { (listing, url) ->
+      launch {
+        val items = service.list(url).await()
+            .filter { it.type == "class" }
+            .map { Item.createForInsert(it.label, it.link, it.deprecated) }
+        itemStore.updateItems(items)
 
-  ITEM_LIST_URL_PATHS.forEach { (listing, url) ->
-    launch {
-      val items = service.list(url).await()
-          .filter { it.type == "class" }
-          .map { Item.createForInsert(it.label, it.link, it.deprecated) }
-      store.updateItems(items)
-
-      println("Updated $listing with ${items.size} items")
+        println("Updated $listing with ${items.size} items")
+      }
     }
   }
 }
