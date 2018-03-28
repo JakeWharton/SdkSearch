cad6b2d158e1c933ec727f9ead34b428b283e6ec
Jake Wharton <jakew@google.com>
2018-03-28 18:43:32 -0400

    Unused imports.

--- debug-updater/src/main/java/com/jakewharton/sdksearch/debug/updater/UpdateActivity.kt
+++ debug-updater/src/main/java/com/jakewharton/sdksearch/debug/updater/UpdateActivity.kt
@@ -2,24 +2,10 @@ package com.jakewharton.sdksearch.debug.updater
 
 import android.app.Activity
 import android.content.Intent
-import android.content.Intent.ACTION_INSTALL_PACKAGE
-import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
-import android.content.Intent.FLAG_GRANT_READ_URI_PERMISSION
 import android.os.Bundle
-import android.support.v4.content.FileProvider
 import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
-import com.jakewharton.sdksearch.api.circleci.CircleCiComponent
-import com.jakewharton.sdksearch.api.circleci.Filter.SUCCESSFUL
-import com.jakewharton.sdksearch.api.circleci.VcsType.GITHUB
 import com.jakewharton.sdksearch.debug.updater.BuildConfig.CIRCLE_CI_TOKEN
-import kotlinx.coroutines.experimental.android.UI
-import kotlinx.coroutines.experimental.launch
-import okio.Okio.buffer
-import okio.Okio.sink
-import timber.log.Timber
-import java.io.File
-import java.io.IOException
 
 class UpdateActivity : Activity() {
   override fun onCreate(savedInstanceState: Bundle?) {
