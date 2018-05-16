277c9130607eb8a6d7497d7fb51ff4de6891c724
Jake Wharton <jakew@google.com>
2018-05-16 18:37:04 -0400

    Port DacModule to Kotlin.

---
+++ api/dac/jdk/src/main/java/com/jakewharton/sdksearch/api/dac/DacModule.kt
@@ -0,0 +1,35 @@
+package com.jakewharton.sdksearch.api.dac
+
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.experimental.CoroutineCallAdapterFactory
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.stringBased
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+import kotlinx.serialization.json.JSON
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+
+@Module
+internal object DacModule {
+  @JvmStatic
+  @Provides
+  @Singleton
+  fun documentationService(baseUrl: BaseUrl, graphClient: OkHttpClient): DocumentationService {
+    val client = graphClient.newBuilder()
+        .addInterceptor(JavascriptSkippingInterceptor())
+        .build()
+
+    val contentType = MediaType.parse("application/json; charset=utf-8")!!
+    val json = JSON.unquoted
+
+    val retrofit = Retrofit.Builder()
+        .baseUrl(baseUrl.url)
+        .client(client)
+        .addConverterFactory(stringBased(contentType, json::parse, json::stringify))
+        .addCallAdapterFactory(CoroutineCallAdapterFactory())
+        .build()
+
+    return retrofit.create(DocumentationService::class.java)
+  }
+}
