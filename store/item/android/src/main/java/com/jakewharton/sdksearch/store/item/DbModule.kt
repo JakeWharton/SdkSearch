ef85f09d3c1e80f9d453b060eca817d2d5242658
Jake Wharton <jakew@google.com>
2018-06-13 23:37:37 -0400

    Port DbModule to Kotlin

---
+++ store/item/android/src/main/java/com/jakewharton/sdksearch/store/item/DbModule.kt
@@ -0,0 +1,27 @@
+package com.jakewharton.sdksearch.store.item
+
+import android.content.Context
+import com.squareup.sqldelight.android.create
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+
+@Module(includes = [DbModule.Providers::class])
+internal abstract class DbModule {
+  @Binds abstract fun itemStore(impl: SqlItemStore): ItemStore
+
+  @Module
+  internal object Providers {
+    @JvmStatic
+    @Provides
+    fun queryWrapper(context: Context, filename: String?): QueryWrapper {
+      return QueryWrapper(QueryWrapper.Companion.create(context, filename))
+    }
+
+    @JvmStatic
+    @Provides
+    fun itemQueries(queryWrapper: QueryWrapper): ItemQueries {
+      return queryWrapper.itemQueries
+    }
+  }
+}
