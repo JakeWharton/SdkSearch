e712ec50361d271b5559f869d70b8658396a237d
Jake Wharton <jakew@google.com>
2018-07-15 00:51:59 -0400

    Beef up the proxy
    * Only proxy from our host
    * Only proxy the resource we care about
    * Add CORS header for our host

--- frontend/web/src/main/proxy.js
+++ frontend/web/src/main/proxy.js
@@ -1,3 +1,33 @@
 addEventListener('fetch', event => {
-  event.respondWith(fetch("https://developer.android.com/_s/getsuggestions?hl=en&p=%2F&s=irina&c=3"));
+  event.respondWith(proxy(event.request));
 })
+
+async function proxy(request) {
+  const referer = request.headers.get('Referer');
+  if (referer && new URL(referer).hostname !== 'sdksearch.app') {
+    // Only proxy requests from the app.
+    return new Response('404 Not Found', { status: 404 });
+  }
+
+  const newUrl = new URL(request.url);
+  if (newUrl.pathname !== '/_s/getsuggestions') {
+    // Proxy only the resource we care about.
+    return new Response('404 Not Found', { status: 404 });
+  }
+  newUrl.host = 'developer.android.com'
+
+  const newRequest = new Request(newUrl, {
+    method: request.method,
+    headers: request.headers,
+  });
+  const response = await fetch(newRequest);
+
+  const newHeaders = new Headers(response.headers);
+  newHeaders.append('Access-Control-Allow-Origin', 'https://sdksearch.app');
+
+  return new Response(response.body, {
+    status: response.status,
+    statusText: response.statusText,
+    headers: newHeaders,
+  });
+}
