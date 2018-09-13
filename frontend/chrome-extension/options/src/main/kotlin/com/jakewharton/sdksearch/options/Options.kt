1b41b327b2ceae6ecc32677c6cd61512e07cc130
Jake Wharton <jakew@google.com>
2018-09-12 23:46:40 -0400

    Use presentation binder helper in extension options

--- frontend/chrome-extension/options/src/main/kotlin/com/jakewharton/sdksearch/options/Options.kt
+++ frontend/chrome-extension/options/src/main/kotlin/com/jakewharton/sdksearch/options/Options.kt
@@ -1,14 +1,13 @@
 package com.jakewharton.sdksearch.options
 
 import com.chrome.platform.Chrome
+import com.jakewharton.presentation.bindTo
 import com.jakewharton.sdksearch.options.presenter.OptionsPresenter
 import com.jakewharton.sdksearch.options.ui.OptionsUiBinder
 import com.jakewharton.sdksearch.reference.PRODUCTION_DAC
 import com.jakewharton.sdksearch.reference.PRODUCTION_GIT_WEB
 import com.jakewharton.sdksearch.store.config.StorageAreaConfigStore
 import kotlinx.coroutines.experimental.Dispatchers
-import kotlinx.coroutines.experimental.GlobalScope
-import kotlinx.coroutines.experimental.launch
 import kotlin.browser.document
 
 fun main(vararg args: String) {
@@ -19,12 +18,6 @@ fun main(vararg args: String) {
   val binder = OptionsUiBinder(document, presenter.events)
 
   document.addEventListener("DOMContentLoaded", {
-    GlobalScope.launch(Dispatchers.Unconfined) {
-      var oldModel: OptionsPresenter.Model? = null
-      for (model in presenter.models) {
-        binder.bind(model, oldModel)
-        oldModel = model
-      }
-    }
+    binder.bindTo(presenter)
   })
 }
