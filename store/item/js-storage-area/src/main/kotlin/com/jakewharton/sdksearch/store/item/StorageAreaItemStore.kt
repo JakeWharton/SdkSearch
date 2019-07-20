48564d634c25fdf44c643714595bc5f33778178b
Jake Wharton <jakew@google.com>
2019-07-19 21:57:18 -0400

    Remove incremental update infrastructure

--- store/item/js-storage-area/src/main/kotlin/com/jakewharton/sdksearch/store/item/StorageAreaItemStore.kt
+++ store/item/js-storage-area/src/main/kotlin/com/jakewharton/sdksearch/store/item/StorageAreaItemStore.kt
@@ -2,12 +2,8 @@ package com.jakewharton.sdksearch.store.item
 
 import com.chrome.platform.storage.StorageArea
 import com.chrome.platform.storage.changes
-import com.chrome.platform.storage.get
 import com.chrome.platform.storage.set
 import com.jakewharton.sdksearch.store.item.Item.Impl
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
@@ -19,56 +15,18 @@ private const val KEY = "items_2"
 
 private fun Item.toPacked() = arrayOf<dynamic>(packageName, className, if (deprecated) 1 else 0, link)
 private fun List<Item>.toPacked() = Array(size) { this[it].toPacked() }
-private fun Map<*, Item>.toPacked() = values.toList().toPacked()
 
 private fun unpackItem(value: dynamic): Item = Impl(-1, value[0], value[1], value[2] == 1, value[3])
 private fun unpackToItemList(value: dynamic) = (value as Array<dynamic>).map(::unpackItem)
-private fun unpackToMutableItemMap(value: dynamic): MutableMap<String, Item> {
-  return unpackToItemList(value).associateByTo(mutableMapOf(), Item::fqcn)
-}
-
-private val Item.fqcn get() = "$packageName.$className"
 
 class StorageAreaItemStore(
   private val storageArea: StorageArea
 ) : ItemStore {
-  private var currentJob: Job? = null
   private val itemsFlow = storageArea.changes(KEY)
       .map { it?.let(::unpackToItemList) ?: emptyList() }
 
-  private suspend fun <T> serially(body: suspend () -> T): T {
-    // TODO replace with actor once available in JS.
-
-    while (true) {
-      currentJob?.join() ?: break
-    }
-    val job = GlobalScope.async {
-      try {
-        body()
-      } finally {
-        currentJob = null
-      }
-    }
-    currentJob = job
-    return job.await()
-  }
-
   override suspend fun updateItems(items: List<Item>) {
-    serially {
-      val existingItems = storageArea.get(KEY)
-
-      val newItems = if (existingItems == null) {
-        items.toPacked()
-      } else {
-        val mutableMap = unpackToMutableItemMap(existingItems)
-        for (item in items) {
-          mutableMap[item.fqcn] = item
-        }
-        mutableMap.toPacked()
-      }
-
-      storageArea.set(KEY, newItems)
-    }
+    storageArea.set(KEY, items.toPacked())
   }
 
   override fun queryItems(term: String): Flow<List<Item>> {
