30e70037df922ed531b14e60e2bd946c1911bce1
Jake Wharton <jakew@google.com>
2019-08-01 16:44:45 -0400

    Update ktlint gradle plugin

--- api/dac-proxy/src/jvmMain/kotlin/com/jakewharton/sdksearch/api/dac/DocumentationService.kt
+++ api/dac-proxy/src/jvmMain/kotlin/com/jakewharton/sdksearch/api/dac/DocumentationService.kt
@@ -1,7 +1,6 @@
 package com.jakewharton.sdksearch.api.dac
 
 import com.jakewharton.sdksearch.proxy.model.DocumentedType
-import kotlinx.coroutines.Deferred
 import retrofit2.http.GET
 
 actual interface DocumentationService {
