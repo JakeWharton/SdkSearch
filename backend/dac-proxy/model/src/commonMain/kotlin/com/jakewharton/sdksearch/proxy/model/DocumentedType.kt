ea999414611f90a6f8423726f19b2ce8c6309fc9
Jake Wharton <jakew@google.com>
2020-03-02 16:38:22 -0500

    Restore "api" property but leave as deprecated

--- backend/dac-proxy/model/src/commonMain/kotlin/com/jakewharton/sdksearch/proxy/model/DocumentedType.kt
+++ backend/dac-proxy/model/src/commonMain/kotlin/com/jakewharton/sdksearch/proxy/model/DocumentedType.kt
@@ -1,11 +1,14 @@
 package com.jakewharton.sdksearch.proxy.model
 
 import kotlinx.serialization.Serializable
+import kotlin.DeprecationLevel.ERROR
 
 @Serializable
 data class DocumentedType(
   val packageName: String,
   val className: String,
   val deprecated: Boolean,
-  val link: String
+  val link: String,
+  @Deprecated("Do not use", level = ERROR)
+  val api: Int = 0
 )
